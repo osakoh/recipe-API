7402d9dcb2bd83122ac5c6045178ac75a4274f15
osakoh <macmive@gmail.com>
2021-08-06 14:00:28 +0100

    git added test for user creation with email

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.contrib import get_user_model
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = "a@a.com"
+        password = "testpass"
+        user = get_user_model().objects.create_user(email=email, password=password)
+        # expected email(email), actual email(user.email)
+        self.assertEqual(user.email, email)
+        # expected password(password), actual password(user.password)
+        self.assertTrue(user.check_password(password))
