fef529f7c45e5c5f61312558dc983b97f692b3cf
osakoh <macmive@gmail.com>
2022-03-13 15:04:37 +0000

    added Tag, Ingredient, & Recipe to admin site

--- app/recipe/admin.py
+++ app/recipe/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
+from .models import Tag, Ingredient, Recipe
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'user']
+    list_filter = ['name', 'user']
+    list_display_links = ['name', 'user']
+    list_per_page = 25
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'user']
+    list_filter = ['name', 'user']
+    list_display_links = ['name', 'user']
+    list_per_page = 25
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['user', 'title', 'time_minutes', 'price', 'link', 'ingredients', 'tags', 'image']
+    list_filter = ['user', 'title', 'time_minutes', 'price', 'link', 'ingredients', 'tags', 'image']
+    list_display_links = ['user', 'title', 'time_minutes', 'price', 'ingredients', 'tags']
+    list_per_page = 25
+
 # Register your models here.
