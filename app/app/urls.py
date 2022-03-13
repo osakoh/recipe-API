ee3619909f9987256f25763e44a2a5ea0343781d
osakoh <macmive@gmail.com>
2022-03-13 14:47:10 +0000

    added receipe url to project url

--- app/app/urls.py
+++ app/app/urls.py
@@ -8,7 +8,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # user app url
     path('api/user/', include('user.urls')),
+    # recipe app url
+    path('api/recipe/', include('recipe.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
