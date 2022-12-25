927e04dd7ad036656bcd1fd70f48e04662e96873
osakoh <macmive@gmail.com>
2022-12-25 23:06:48 +0000

    routes for swagger & openapi schema

--- app/app/urls.py
+++ app/app/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,6 +13,15 @@ urlpatterns = [
     path('api/user/', include('user.urls')),
     # recipe app url
     # path('api/recipe/', include('recipe.urls')),
+
+    # drf_spectacular urls
+
+    # generates the schema: YAML file
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # swagger doc to generate documentation
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='api-docs'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='api-redoc'),
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
